bda8a1a548d4f44f6735da17f22290b3acdd82b0
Leandro Heidemann da Silva <leandrohds_bn@hotmail.com>
2022-03-09 23:20:03 -0300

    first commit

---
+++ src/main/java/com/github/leandroheidemann/springgraphqltest/SpringGraphqlTestApplication.java
@@ -0,0 +1,13 @@
+package com.github.leandroheidemann.springgraphqltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphqlTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphqlTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/leandroheidemann/springgraphqltest/SpringGraphqlTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.leandroheidemann.springgraphqltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphqlTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
